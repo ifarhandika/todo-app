71299325e6ee895e69609054af620276f66245c7
ifarhandika <ifarhandika@gmail.com>
2021-06-01 11:45:04 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { useLocation } from "react-router"
+import Button from "./Button"
+
+const Header = ({ onAdd, showAdd }) => {
+  const location = useLocation()
+
+  return (
+    <header className="header">
+      <h1>Task Tracker</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
+}
+
+export default Header
